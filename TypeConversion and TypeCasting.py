449d71c8ab3c5c39a1a3e96b6dac9416df56302a
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-05-27 13:45:38 +0530

    Add files via upload
    Learnt type conversion and type casting

---
+++ TypeConversion and TypeCasting.py
@@ -0,0 +1,15 @@
+#type conversion
+#automatically
+a = 10
+b = 1.5
+sum1 = a+b
+print("The sum is:", sum1)
+
+#type casting
+#manually
+c,d=10,int("2")
+
+sum2 = c + d
+print("The sum is:", sum2)
+
+

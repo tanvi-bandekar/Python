0b08c9d726444d7f65a29f3a20bdf06b90271881
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-05-27 14:15:04 +0530

    Add files via upload
    Accept value from user and display their difference

---
+++ PrintDiff.py
@@ -0,0 +1,4 @@
+#code to accept integers and find their difference
+a = int(input("enter integer to subtract from :"))
+b = int(input("enter integer to be subtracted :"))
+print("The difference is: ", a-b)

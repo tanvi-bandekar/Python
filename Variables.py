fa9f5d2c33e8b0b49a1c9297fcef9a3f6cf95868
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-05-27 12:15:08 +0530

    Add files via upload
    Learnt the use of variables, how to name them, store value in them and display them

---
+++ Variables.py
@@ -0,0 +1,11 @@
+name = "Tanvi" 
+age = 18
+time = 1.23
+age2 = age
+print(name)
+print(age)
+print(time)
+print("My name is :",name)
+print("My age is :",age)
+print("My age is :",age2)
+print("My time taken in seconds is :",time)

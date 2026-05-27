16087427981d7acda62b6ae902d4e0729f7a6e77
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-05-27 14:07:17 +0530

    Add files via upload
    Learnt to accept values from user

---
+++ Inputs.py
@@ -0,0 +1,11 @@
+input("enter your name:")
+name = input("enter your fathers name:")
+print("Welcome ",name)
+
+#taking value other than string
+
+val = input("enter some random value:")
+print(type(val),"you entered: ",val)
+
+val = int(input("enter some random value:"))
+print(type(val),"you entered: ",val) #now we get the data type of val as int

63fef5be3829765d1d807b5e408f224ee2db31e4
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-05-27 12:41:31 +0530

    Add files via upload
    Learnt data types and also a new thing that string literals can be written within single, double and even triple qoutes. Preferred to use double qoutes.

---
+++ DataTypes.py
@@ -0,0 +1,17 @@
+name1 = "Tanvi" 
+name2 = 'Tanvi'
+name3 = '''Tanvi'''
+age = 18
+old = False
+beta = None
+time = 1.23
+print(name1)
+print(name2)
+print(name3)
+
+print(type(name1))
+print(type(age))
+print(type(time))
+print(type(old))
+print(type(beta))
+

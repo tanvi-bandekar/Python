6ea451d119143c0bbd13671b866e4c872772d638
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-05-28 21:57:23 +0530

    Add files via upload
    A few important string functions

---
+++ StringFunctions.py
@@ -0,0 +1,7 @@
+str = "I am Tanvi."
+print(str.endswith("vi."))
+print(str.capitalize())
+print(str.replace("I am","Hi there, I am"))
+print((str.find("a")))
+print((str.find("L")))
+print((str.count("am")))

ae1f5fea534d4e63bf7e3b1da10517d1cf6ec62a
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-05-28 16:51:17 +0530

    Add files via upload
    Learnt String operations, indexing and slicing

---
+++ StringOperations.py
@@ -0,0 +1,11 @@
+str1 = "denceeee"
+print(len(str1)) #string 1 length calculation
+str2 = "moreeeeeeeeeeeeeee1"
+print(len(str2)) #string 2 length calculation
+print(str1+str2) #concatenation
+print(str1[1]) #indexing
+print(str2[1:3]) #slicing
+print(str2[:3]) #slicing [0:3]
+print(str2[1:]) #slicing [1:len(str)]
+print(str1[-6:-1])
+

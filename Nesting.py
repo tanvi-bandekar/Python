f7727213efcc214c949ed2981c360521c22ae40d
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-06-01 11:14:21 +0530

    Add files via upload
    Program showcasing nesting of conditional statements

---
+++ Nesting.py
@@ -0,0 +1,8 @@
+age = int(input("enter your age: "))
+if(age>=18):
+    if(age>=80):
+        print("you cannot drive")
+    else:
+        print("you can drive")
+else:
+    print("you cannot drive")

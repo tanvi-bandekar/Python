f8a1e2c4d94de743825903a7f80427b4a4db21b5
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-06-01 13:48:19 +0530

    Add files via upload
    How to create a tuple

---
+++ Tuples.py
@@ -0,0 +1,14 @@
+tup = (91,3,4)
+print(tup)
+tup1 = ()
+print(tup1)
+tup2 =(1,) #comma necessary for class to be tuple
+print(tup2)
+print(type(tup2))
+tup3 =(1) #considered as int
+print(tup3)
+print(type(tup3))
+tup4 =(1,2,3) #comma not necessary at the end
+print(tup4)
+print(type(tup4))
+#tuple slicing can be done too

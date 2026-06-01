907cc000ddc318101efb31b3b29e064f2ee829c0
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-06-01 11:08:31 +0530

    Add files via upload
    Accept marks (integer) from user and assign grade

---
+++ ConditionMarks.py
@@ -0,0 +1,9 @@
+marks = int(input("enter your marks:"))
+if(marks>=90):
+    print("Grade A")
+elif(90>marks>=80):
+    print("Grade B")
+elif(80>marks>=70):
+    print("Grade C")
+else:
+    print("Grade D")

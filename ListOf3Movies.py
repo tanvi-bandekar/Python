e28ea72a85bd825206940e21176a8408f364cf34
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-06-01 14:11:52 +0530

    Add files via upload
    Accept 3 movie names and display List

---
+++ ListOf3Movies.py
@@ -0,0 +1,7 @@
+a = input("enter name of first movie: ")
+b = input("enter name of second movie: ")
+c = input("enter name of third movie: ")
+list = [a, b, c]
+print(list)
+
+#can also use list.append so that only one variable is used

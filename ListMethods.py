af3d86b0cafbafddc7cfd04339a82978d68ca9f8
Tanvi Deepak Bandekar <tanvi.bandekar@vit.edu.in>
2026-06-01 13:29:00 +0530

    Add files via upload
    Some common List Methods

---
+++ ListMethods.py
@@ -0,0 +1,16 @@
+list = [2,6,1]
+list.append(9) #insert value at end
+print(list)
+list.sort() #ascending order
+print(list)
+list.sort(reverse=True) #descending order
+print(list)
+list.reverse() 
+print(list)
+list.insert(2,7) #(index,element)
+print(list)
+
+list1 = [ 2, 1, 3 ,1 ]
+list1.remove(1) #remove first occurence of the element
+print(list1)
+list1.pop(2) #removes element at index
